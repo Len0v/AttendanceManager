00783f33c9847649b6575af71a9ccb013615f583
Len0v <robal1xxx@gmail.com>
2017-05-24 22:46:31 +0200

    PCL fix

--- AttendanceManagerClient/SmartCardPCL/ElectronicStudentCardContactDataReader.cs
+++ AttendanceManagerClient/SmartCardPCL/ElectronicStudentCardContactDataReader.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using Core.Smartcard;
-using Readers;
+using SmartCardPCL.Readers;
 
 namespace SmartCardPCL
 {
